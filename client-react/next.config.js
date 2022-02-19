6b0593fee84b690cefe692318e09961c09d4b171
Asperun <carnageepl@gmail.com>
2022-02-19 16:23:43 +0100

    Using experimental minifier

--- client-react/next.config.js
+++ client-react/next.config.js
@@ -1,4 +1,9 @@
 module.exports = {
+  swcMinify: true,
+  reactStrictMode: true,
+  experimental: {
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
   images: {
     domains: ["cdn.jetphotos.com","hatscripts.github.io","cdn.planespotters.net"],
   },
