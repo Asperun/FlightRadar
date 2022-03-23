dd42d0c67f2e58b26d37b1c975cf397a5036f214
Asperun <carnageepl@gmail.com>
2022-03-23 22:43:47 +0100

    Migrated to Postgres

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,59 +1,54 @@
 using System.IO.Compression;
 using System.Net;
-using System.Security.Cryptography.X509Certificates;
-using System.Text.Encodings.Web;
 using System.Text.Json;
 using FlightRadar.Data;
 using FlightRadar.Services;
 using FlightRadar.Tasks;
-using Microsoft.AspNetCore.Authentication.Certificate;
+using FlightRadar.Util;
 using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
-builder.Services.AddAuthentication(CertificateAuthenticationDefaults.AuthenticationScheme).AddCertificate();
-builder.Services.AddControllers().AddJsonOptions(options =>
-{
-    options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
-    options.JsonSerializerOptions.Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping;
-    // options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve;
-});
+Globals.ConnectionString = builder.Environment.IsDevelopment()
+                               ? builder.Configuration.GetConnectionString("DevDb")
+                               : builder.Configuration.GetConnectionString("Postgres");
+
+builder.Services.AddControllers().AddJsonOptions(options => { options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase; });
 builder.Services.AddHttpClient<PlanesFetcher>("OpenSky");
-builder.Services.AddDbContext<PlaneContext>(x => x.UseSqlServer(connectionString));
+builder.Services.AddDbContextPool<PlaneContext>(opt => opt.UseNpgsql(Globals.ConnectionString));
+AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
 builder.Services.AddScoped<PlaneService>();
 builder.Services.AddSingleton<PlaneBroadcaster>();
 
 // ====== Swagger ======
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+}
 
 // ====== Async Background Tasks ======
 builder.Services.AddHostedService<PlanesFetcher>();
+
 // ====== HTTP Protocols ======
 builder.WebHost.ConfigureKestrel((context, options) =>
 {
-    // options.Listen(IPAddress.Any, 5001, listenOptions =>
-    options.Listen(IPAddress.Any, 443, listenOptions =>
-    {
-        // listenOptions.Protocols = HttpProtocols.Http1AndHttp2;
-        listenOptions.UseHttps(StoreName.My,"fantasea.pl",false,StoreLocation.CurrentUser);
-    });
+    options.Listen(IPAddress.Any, 5001, listenOptions => { listenOptions.Protocols = HttpProtocols.Http1AndHttp2; });
 });
 
 // ====== Compression ======
 builder.Services.AddResponseCompression(options =>
 {
-    options.EnableForHttps = true;
-    // options.Providers.Add<GzipCompressionProvider>();
+    // options.EnableForHttps = true;
     options.Providers.Add<BrotliCompressionProvider>();
+    options.Providers.Add<GzipCompressionProvider>();
     options.MimeTypes = new[] { "text/plain", "text/event-stream", "application/json" };
 });
 
 builder.Services.Configure<BrotliCompressionProviderOptions>(options => { options.Level = CompressionLevel.Fastest; });
-
+builder.Services.Configure<GzipCompressionProviderOptions>(options => { options.Level = CompressionLevel.Fastest; });
 
 var app = builder.Build();
 
@@ -67,26 +62,14 @@ if (app.Environment.IsDevelopment())
 
 app.UseResponseCompression();
 app.UseRouting();
-// app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseCors(b => b.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader().Build());
+// app.UseCors(b => b.SetIsOriginAllowedToAllowWildcardSubdomains());
 app.UseAuthorization();
 app.UseEndpoints(endpoint => endpoint.MapControllers());
 
-
 // ====== Migrations ======
 var planeCtx = app.Services.CreateScope().ServiceProvider.GetRequiredService<PlaneContext>();
-
-if (planeCtx.Database.GetPendingMigrations().Any())
-    planeCtx.Database.Migrate();
-
 planeCtx.Database.EnsureCreated();
+
 app.Run();
-
-/* ################################ IMPORTANT ########################################################################################################
-Console.WriteLine(planeCtx.Flights.First().Checkpoints.Count); // Doesnt work because Lists are lazily fetched(has to be accessed in same scope)
-
-Console.WriteLine(planeCtx.Flights.Select(fl => fl.Checkpoints).First().Count); // Works because we specifically select List to be fetched with query.
-Console.WriteLine(planeCtx.Planes.Include(p => p.Flights).First()); // Works because we specifically include List to be fetched with query.
-Console.WriteLine(planeCtx.Planes.Include(p => p.Flights).ThenInclude(f =>f.Checkpoints).First()); // Works because we specifically chain what has to be fetched with query
-####################################################################################################################################################### */
